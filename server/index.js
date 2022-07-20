7302fc263014a219c045613b65b0d0f855559046
Neelanjan Mukherji <neelanjanmukherji@gmail.com>
2022-07-20 12:53:50 +0530

    Inital Commit with Mongoose Setup and GraphQL Setup and both are connected

---
+++ server/index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const colors = require('colors')
+const { graphql } = require('graphql')
+const schema = require('../server/schema/schema')
+const { graphqlHTTP } = require('express-graphql')
+const connectDB = require('./config/db')
+require('dotenv').config()
+
+const port = process.env.PORT || 8000
+
+url = `http://localhost:${port}/`.bgBlue
+
+const  colored_port = `${port}`.bgBlack
+
+
+// MongoDB Connection
+connectDB()
+
+const app = express()
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql :process.env.NODE_ENV === 'development'
+}))
+
+
+app.listen(port, console.log(`Server is running on ${url} and the port is ${colored_port}`))
